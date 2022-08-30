7a1529073034784764394e31f73d28fe7bb99333
İlker BALTACI <ilkerballtaci@gmail.com>
2022-08-30 19:26:19 +0300

    Proje ismi güncellendi. g++ dependency linux makineye yükleniyor.

--- build_all.sh
+++ build_all.sh
@@ -19,9 +19,9 @@ mkdir -p ./cross-platform-binaries/windows_x64
 mkdir -p ./cross-platform-binaries/linux_x86_64
 mkdir -p ./cross-platform-binaries/linux_aarch64
 
-cp ./build-Win32/bin/HelloWorld.exe ./cross-platform-binaries/windows_x86
-cp ./build-Win64/bin/HelloWorld.exe ./cross-platform-binaries/windows_x64
-cp ./build-linux_x86_64/bin/HelloWorld ./cross-platform-binaries/linux_x86_64
-cp ./build-linux_aarch64/bin/HelloWorld ./cross-platform-binaries/linux_aarch64
+cp ./build-Win32/bin/CMake-CrossCompile-HelloWorld.exe ./cross-platform-binaries/windows_x86
+cp ./build-Win64/bin/CMake-CrossCompile-HelloWorld.exe ./cross-platform-binaries/windows_x64
+cp ./build-linux_x86_64/bin/CMake-CrossCompile-HelloWorld ./cross-platform-binaries/linux_x86_64
+cp ./build-linux_aarch64/bin/CMake-CrossCompile-HelloWorld ./cross-platform-binaries/linux_aarch64
 
 
